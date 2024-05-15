727bc0edc82f022d6f761aac51b056c2d96cce8c
camisbersann <camila.bersan@aluno.senai.br>
2024-05-15 15:34:04 -0300

    fix: add informations

--- src/components/ImageDrawer/index.jsx
+++ src/components/ImageDrawer/index.jsx
@@ -1,5 +1,6 @@
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
-import { ImageBackground, View, Text, Image } from 'react-native';
+import { ImageBackground, View, Text, Image, TouchableOpacity } from 'react-native';
+import { Divider } from '@rneui/themed';
 
 const Drawer = createDrawerNavigator();
 
@@ -10,6 +11,7 @@ const CustomDrawerContent = (props) => {
           <Image source={require('../../../assets/logo.png')} style={{ width: 120, height: 120 }} />
         </View>
       <DrawerItemList {...props} />
+      <Divider orientation="horizontal" width={1} color="white" style={{borderBottomWidth: 1,  marginVertical: 10, width: 200, marginLeft: 30}} />
     </DrawerContentScrollView>
   );
 };
