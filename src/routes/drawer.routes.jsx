a0874b5ea18068d9271f66fe94f4e3071ae2afe5
camisbersann <camila.bersan@aluno.senai.br>
2024-05-15 11:22:07 -0300

    feat: options ArtesPlasticas

--- src/routes/drawer.routes.jsx
+++ src/routes/drawer.routes.jsx
@@ -164,7 +164,32 @@ const DrawerRoutes = () => {
           textColor: "#fff",
         },
       }}/>
-      <Drawer.Screen name="ArtesPlasticas" component={ArtesPlasticas} />
+      <Drawer.Screen name="ArtesPlasticas" component={ArtesPlasticas} 
+      options={{
+        title: "Artes Plásticas",
+        headerTitle: "",
+        headerTitle: () => (
+          <View style={{ flexDirection: "row", alignItems: "right" }}>
+            <Image
+              source={require("../../assets/logo.png")}
+              style={{
+                width: 55,
+                height: 55,
+                marginLeft: 270,
+                marginBottom: 20,
+                marginTop: 20,
+              }}
+            />
+          </View>
+        ),
+        headerStyle: {
+          backgroundColor: "#04233f",
+        },
+        drawerStyle: {
+          backgroundColor: "#04233f",
+          textColor: "#fff",
+        },
+      }}/>
       <Drawer.Screen name="SobreNos" component={SobreNos} />
     </Drawer.Navigator>
   );
