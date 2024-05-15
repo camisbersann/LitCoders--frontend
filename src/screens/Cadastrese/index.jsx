785a9d0ac06e4c5a1fb2ae56cbef906f70d6ba3d
unknown <nicoly.ferreira@aluno.senai.br>
2024-05-15 09:59:36 -0300

    card com inputs

--- src/screens/Cadastrese/index.jsx
+++ src/screens/Cadastrese/index.jsx
@@ -1,14 +1,29 @@
-import { View, Text, TouchableOpacity, Image, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, Image, ScrollView, Link,  } from 'react-native';
 import styles from './styles';
 import Title from '../../components/Title';
 
 export default function Cadastrese() {
   return (
-    <ScrollView>
     <View style={styles.container}>
+      <ScrollView>
       <Text style={styles.text}>Cadastre-se</Text>
-
+      <View style={styles.cardcadastro}>
+        <Text style={styles.textButton}>Nome:</Text>
+        <Text style={styles.textButton}>Sobrenome:</Text>
+        <Text style={styles.textButton}>Data de Nascimento:</Text>
+        <Text style={styles.textButton}>CPF:</Text>
+        <Text style={styles.textButton}>Telefone:</Text>
+        <Text style={styles.textButton}>Email:</Text>
+        <Text style={styles.textButton}>Senha:</Text>
+        <Text style={styles.textButton}>Confirme a senha:</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton2}>Cadastrar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton2}>Voltar</Text>
+        </TouchableOpacity>
+      </View>
+      </ScrollView>
     </View>
-    </ScrollView>
   )
 }
--- src/screens/Cadastrese/styles.js
+++ src/screens/Cadastrese/styles.js
@@ -11,5 +11,21 @@ const styles = StyleSheet.create({
         textAlign: "center",
         marginTop: 20,
     },
+    cardcadastro: {
+        backgroundColor: "#F3DC8C",
+        padding: 20,
+        margin: 20,
+        borderRadius: 10,
+    },
+    button: {
+        backgroundColor: "#8CA2B0",
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10,
+    },
+    textButton2: {
+        color: "#FFF",
+        textAlign: "center",
+    },
 });
 export default styles;
