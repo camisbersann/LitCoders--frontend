87cbd576f3fc80acb68849aa5993cf7bbde1c70c
manuxcamargo <manuxcamargo.564@gmail.com>
2024-05-15 09:39:29 -0300

    primeiras alterações

---
+++ src/components/SobreNos/index.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+const SobreNos = () => {
+  return (
+    <View>
+      <Text style={styles.sobreNos}>Sobre Nós</Text>
+
+      <View style={styles.anaContainer}>
+        <Text style={styles.anaNome}>Ana Cavalcante</Text>
+        <Text style={styles.anaCargo}>Desempregada</Text>
+        <Text style={styles.anaBio}>
+          Tenho 17 anos, moro em Campinas-SP, e curso Desenvolvimento de
+          Sistemas no SENAI Valinhos.
+        </Text>
+      </View>
+
+      <View style={styles.andreyContainer}>
+        <Text style={styles.andreyNome}>Andrey Botero</Text>
+        <Text style={styles.andreyCargo}>Desempregado</Text>
+        <Text style={styles.andreyBio}>
+          Tenho 17 anos, moro em Campinas-SP, e curso Desenvolvimento de
+          Sistemas no SENAI Valinhos.
+        </Text>
+      </View>
+
+      <View style={styles.camilaContainer}>
+        <Text style={styles.camilaNome}>Camila Bersan</Text>
+        <Text style={styles.camilaCargo}>Desempregada</Text>
+        <Text style={styles.camilaBio}>
+          Tenho 17 anos, moro em Campinas-SP, e curso Desenvolvimento de
+          Sistemas no SENAI Valinhos.
+        </Text>
+      </View>
+
+      <View style={styles.fabioContainer}>
+        <Text style={styles.fabioNome}>Fabio Junior</Text>
+        <Text style={styles.fabioCargo}>Desempregada</Text>
+        <Text style={styles.fabioBio}>
+          Tenho 17 anos, moro em Campinas-SP, e curso Desenvolvimento de
+          Sistemas no SENAI Valinhos.
+        </Text>
+      </View>
+      <View style={styles.anaContainer}>
+        <Text style={styles.anaNome}>Ana Cavalcante</Text>
+        <Text style={styles.anaCargo}>Desempregada</Text>
+        <Text style={styles.anaBio}>
+          Tenho 17 anos, moro em Campinas-SP, e curso Desenvolvimento de
+          Sistemas no SENAI Valinhos.
+        </Text>
+      </View>
+      <View style={styles.anaContainer}>
+        <Text style={styles.anaNome}>Ana Cavalcante</Text>
+        <Text style={styles.anaCargo}>Desempregada</Text>
+        <Text style={styles.anaBio}>
+          Tenho 17 anos, moro em Campinas-SP, e curso Desenvolvimento de
+          Sistemas no SENAI Valinhos.
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default SobreNos;
