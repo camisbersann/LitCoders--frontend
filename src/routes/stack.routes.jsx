d49dd9cd580de19421ba06ee095100e4cf4afbc5
unknown <nicoly.ferreira@aluno.senai.br>
2024-05-15 09:32:17 -0300

    arrumando routes

--- src/routes/stack.routes.jsx
+++ src/routes/stack.routes.jsx
@@ -1,8 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "../screens/Home";
-import Profile from "../screens/Profile";
-import Category from "../screens/Category";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +9,7 @@ const StackRoutes = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="Category" component={Category} />
+
     </Stack.Navigator>
   );
 };
--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -1,8 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import Home from "../screens/Home";
-import Profile from "../screens/Profile";
-import Category from "../screens/Category";
+
 
 const Tab = createBottomTabNavigator();
 
@@ -10,8 +9,7 @@ const TabRoutes = () => {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Category" component={Category} />
+
     </Tab.Navigator>
   );
 };
