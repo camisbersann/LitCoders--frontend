ff4be005a4a0d492241b7f10ddf4cd4795d19e61
unknown <nicoly.ferreira@aluno.senai.br>
2024-05-15 09:47:32 -0300

    adicionando a scrollview

--- src/screens/Cadastrese/index.jsx
+++ src/screens/Cadastrese/index.jsx
@@ -1,11 +1,14 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image, ScrollView } from 'react-native';
 import styles from './styles';
 import Title from '../../components/Title';
 
 export default function Cadastrese() {
   return (
+    <ScrollView>
     <View style={styles.container}>
       <Text style={styles.text}>Cadastre-se</Text>
+
     </View>
+    </ScrollView>
   )
 }
